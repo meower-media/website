33325c3789e93edfb3a5c188bf8083dabaa1090c
M.D. Walters <67456566+Mwalters75@users.noreply.github.com>
2022-04-29 08:46:14 -0400

    Fix delayed header

--- header-text.js
+++ header-text.js
@@ -1,3 +1,5 @@
 function applyHeader() {
     document.getElementById("header").innerHTML = '<div id="scratch" class="logo" href="/"><a id="scratch" href="/">Meower </a><a id="scratch" class="link" href="https://app.meower.org">launch</a> <a id="scratch" class="link" href="https://wiki.meower.org">wiki</a> <a id="scratch" class="link" href="https://beta.meower.org">beta</a> <a id="scratch" class="link" href="/legal.html">legal</a></div>'; 
 }
+
+applyHeader();
