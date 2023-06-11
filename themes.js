1599ad6fcf11041136c401d0fd082e2da20ecf40
DaCatBlock <89345822+HAZARD6896@users.noreply.github.com>
2023-06-11 14:47:41 -0400

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,28 +1,30 @@
 // Function to switch theme
-    function switchTheme(themeName) {
-      document.documentElement.className = themeName;
-      localStorage.setItem('theme', themeName);
-    }
+function switchTheme(themeName) {
+  document.documentElement.className = themeName;
+  localStorage.setItem('theme', themeName);
+}
 
-    document.getElementById('light-theme-1-btn').addEventListener('click', function () {
-      switchTheme('light-theme-1');
-    });
+document.getElementById('light-theme-1-btn').addEventListener('click', function () {
+  switchTheme('light-theme-1');
+});
 
-    document.getElementById('light-theme-2-btn').addEventListener('click', function () {
-      switchTheme('light-theme-2');
-    });
+document.getElementById('light-theme-2-btn').addEventListener('click', function () {
+  switchTheme('light-theme-2');
+});
 
-    document.getElementById('dark-theme-1-btn').addEventListener('click', function () {
-      switchTheme('dark-theme-1');
-    });
+document.getElementById('dark-theme-1-btn').addEventListener('click', function () {
+  switchTheme('dark-theme-1');
+});
 
-    document.getElementById('dark-theme-2-btn').addEventListener('click', function () {
-      switchTheme('dark-theme-2');
-    });
+document.getElementById('dark-theme-2-btn').addEventListener('click', function () {
+  switchTheme('dark-theme-2');
+});
 
-    document.addEventListener('DOMContentLoaded', (event) => {
-      const savedTheme = localStorage.getItem('theme');
-      if (savedTheme) {
-        switchTheme(savedTheme);
-      }
-    });
+document.addEventListener('DOMContentLoaded', (event) => {
+  let savedTheme = localStorage.getItem('theme');
+  if (!savedTheme) {
+    savedTheme = 'light-theme-1';
+    localStorage.setItem('theme', savedTheme);
+  }
+  switchTheme(savedTheme);
+});
