dd02b952727bac059d9c51d4f0124be74b93f569
DaCatBlock <89345822+HAZARD6896@users.noreply.github.com>
2023-06-12 15:47:01 -0400

    Update themes.js
    Changing placement of Themes button to a toggle in the footer

--- themes.js
+++ themes.js
@@ -1,30 +1,32 @@
+// Array of theme names
+const themes = ['light-theme-1', 'light-theme-2', 'dark-theme-1', 'dark-theme-2'];
+// Variable to store the index of the current theme
+let currentThemeIndex = 0;
+
 // Function to switch theme
 function switchTheme(themeName) {
   document.documentElement.className = themeName;
   localStorage.setItem('theme', themeName);
 }
 
-document.getElementById('light-theme-1-btn').addEventListener('click', function () {
-  switchTheme('light-theme-1');
-});
+// Function to switch to the next theme
+function switchToNextTheme() {
+  // Increment the current theme index, wrapping around to 0 if it reaches the number of themes
+  currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+  // Switch to the new theme
+  switchTheme(themes[currentThemeIndex]);
+}
 
-document.getElementById('light-theme-2-btn').addEventListener('click', function () {
-  switchTheme('light-theme-2');
-});
-
-document.getElementById('dark-theme-1-btn').addEventListener('click', function () {
-  switchTheme('dark-theme-1');
-});
-
-document.getElementById('dark-theme-2-btn').addEventListener('click', function () {
-  switchTheme('dark-theme-2');
-});
+// Add event listener to 'Change Theme' button
+document.getElementById('theme-switcher-button').addEventListener('click', switchToNextTheme);
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  let savedTheme = localStorage.getItem('theme');
-  if (!savedTheme) {
-    savedTheme = 'light-theme-1';
-    localStorage.setItem('theme', savedTheme);
+  // Retrieve the saved theme from localStorage
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    // If a theme was saved, set it as the current theme
+    switchTheme(savedTheme);
+    // Also update the current theme index
+    currentThemeIndex = themes.indexOf(savedTheme);
   }
-  switchTheme(savedTheme);
 });
