8ed3bb2588e50ec06e20da7efc3fb2b07a970c58
DaCatBlock <89345822+HAZARD6896@users.noreply.github.com>
2023-06-12 15:56:31 -0400

    Update themes.js
    fixed to use light orange theme by default

--- themes.js
+++ themes.js
@@ -1,32 +1,38 @@
-// Array of theme names
-const themes = ['light-theme-1', 'light-theme-2', 'dark-theme-1', 'dark-theme-2'];
-// Variable to store the index of the current theme
-let currentThemeIndex = 0;
-
 // Function to switch theme
 function switchTheme(themeName) {
   document.documentElement.className = themeName;
   localStorage.setItem('theme', themeName);
 }
 
-// Function to switch to the next theme
-function switchToNextTheme() {
-  // Increment the current theme index, wrapping around to 0 if it reaches the number of themes
-  currentThemeIndex = (currentThemeIndex + 1) % themes.length;
-  // Switch to the new theme
-  switchTheme(themes[currentThemeIndex]);
-}
+// Get all themes in an array
+const themes = ['light-theme-1', 'light-theme-2', 'dark-theme-1', 'dark-theme-2'];
 
-// Add event listener to 'Change Theme' button
-document.getElementById('theme-switcher-button').addEventListener('click', switchToNextTheme);
+// Get theme switcher button
+let themeSwitcherButton = document.getElementById('theme-switcher-button');
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  // Retrieve the saved theme from localStorage
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme) {
-    // If a theme was saved, set it as the current theme
-    switchTheme(savedTheme);
-    // Also update the current theme index
-    currentThemeIndex = themes.indexOf(savedTheme);
+// Event listener for the theme switcher button
+themeSwitcherButton.addEventListener('click', function () {
+  // Get current theme from local storage
+  let currentTheme = localStorage.getItem('theme');
+
+  // If there's no theme set in local storage, set it to the first theme
+  if (!currentTheme) {
+    currentTheme = themes[0];
   }
+
+  // Find the index of the current theme in our array
+  let currentIndex = themes.indexOf(currentTheme);
+
+  // If current theme is the last in the array, loop back to the first theme
+  // Else, move to the next theme
+  let nextIndex = (currentIndex + 1) % themes.length;
+
+  // Switch to the next theme
+  switchTheme(themes[nextIndex]);
+});
+
+// On page load, set theme to saved theme or the first theme if no theme is saved
+document.addEventListener('DOMContentLoaded', (event) => {
+  let savedTheme = localStorage.getItem('theme') || themes[0];
+  switchTheme(savedTheme);
 });
