81d3ec3cfd70b8f3e5dee4afebd4538b7590db4a
AtomicInitial <143096875+AtomicInitial@users.noreply.github.com>
2023-09-17 21:54:13 -0400

    Update live.js

--- live.js
+++ live.js
@@ -18,39 +18,13 @@ function getFetch(url) { // https://www.johnlivingston.io/blog/aync-await-fetch
 
 let json;
 
-function addLoop(amt, index, type) {
-  setTimeout(() => {
-    let i = index;
-    i++;
-    if (i <= amt) {
-      if (type == "posts") {
-        document.getElementById(type).setAttribute("data-value", i);
-      }
-      if (type == "users") {
-        document.getElementById(type).setAttribute("data-value", i);
-      }
-      if (type == "chats") {
-        document.getElementById(type).setAttribute("data-value", i);
-      }
-      addLoop(amt, i, type)
-    }
-  }, 500)
-}
-
 function getStats() {
   getFetch("https://api.meower.org/statistics").then((results) => {
     json = results;
 
-    if (document.getElementById("posts").getAttribute("data-value") == "------") {
-      document.getElementById("posts").setAttribute("data-value", json.posts);
-      document.getElementById("users").setAttribute("data-value", json.users);
-      document.getElementById("chats").setAttribute("data-value", json.chats);
-    }
-    else {
-      addLoop(json.posts, parseInt(document.getElementById("posts").getAttribute("data-value"), 10), "posts");
-      addLoop(json.users, parseInt(document.getElementById("users").getAttribute("data-value"), 10), "users");
-      addLoop(json.chats, parseInt(document.getElementById("chats").getAttribute("data-value"), 10), "chats");
-    }
+    document.getElementById("posts").setAttribute("data-value", json.posts);
+    document.getElementById("users").setAttribute("data-value", json.users);
+    document.getElementById("chats").setAttribute("data-value", json.chats);
     setTimeout(getStats, 2000)
   })
 }
